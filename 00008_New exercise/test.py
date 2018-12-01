57dc0fb1c374dbb9e910295b8dd310b92a063be1
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-01 09:50:37 -0500

    Mumuki Export on 2018-12-01 09:50:37 -0500

--- 00008_New exercise/test.py
+++ 00008_New exercise/test.py
@@ -4,6 +4,3 @@ class TestFixtures(unittest.TestCase):
     
   def test_listas(self):
     self.assertEquals(saludos, ["Hola Angeles!", "Hola Carla!", "Hola Natalia!", "Hola Eugenia!"])
-
-  def test_listas(self):
-    self.assertTrue('append' in '/*...content...*/')
