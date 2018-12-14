8b4fa32d28d9f3aa498511953743d497dbcd4c1a
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 15:50:56 -0500

    Mumuki Export on 2018-12-14 15:50:56 -0500

--- 00008_Reforzando lo que vimos/test.py
+++ 00008_Reforzando lo que vimos/test.py
@@ -1,7 +1,16 @@
 class TestFixtures(unittest.TestCase):
   
-  
-  
+  def test_contenido(self):
+    
+    def f(saludos_test, saludos):
+      for i in saludos:
+        if i not in saludos_test:
+          return False
+      for i in saludos_test:
+        if i not in saludos:
+          return False
+      return True
+    self.assert(f(saludos_test, saludos), "Algun saludo no es correcto o se repite")
   def test_listas(self):
     saludos_test = ["Hola " + x + "!" for x in nombres]
     self.assertEquals(len(saludos_test), len(saludos))
