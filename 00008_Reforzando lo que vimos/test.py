c3062cc83e4389174117392cfda9c009534987e1
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 15:51:59 -0500

    Mumuki Export on 2018-12-14 15:51:59 -0500

--- 00008_Reforzando lo que vimos/test.py
+++ 00008_Reforzando lo que vimos/test.py
@@ -10,6 +10,7 @@ class TestFixtures(unittest.TestCase):
         if i not in saludos:
           return False
       return True
+    saludos_test = ["Hola " + x + "!" for x in nombres]
     self.assertTrue(f(saludos_test, saludos), "Algun saludo no es correcto o se repite")
   
   def test_listas(self):
