e21bc825da0976e890231d45cde2ccf8087d02d5
Andres Furlan <Andres Furlan>
2019-02-06 23:43:51 +0000

    Mumuki Export on 2019-02-06 23:43:51 +0000

--- 00003_Iteración III/test.py
+++ 00003_Iteración III/test.py
@@ -9,4 +9,4 @@ class TestFixtures(unittest.TestCase):
       
       return True
       
-    self.assertTrue(estan_los_cuadrados(lista1,cuadrados), 'Los elementos no son el cuadrado de los numeros dados')
+    self.assertTrue(estan_los_cuadrados(lista1,cuadrados), 'Al menos un elemento no es un cuadrado.')
