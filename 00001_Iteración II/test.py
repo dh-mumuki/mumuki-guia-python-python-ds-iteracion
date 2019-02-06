1d85ed51030c812b8172254a440ea6bf0a194b93
Andres Furlan <Andres Furlan>
2019-02-06 23:35:29 +0000

    Mumuki Export on 2019-02-06 23:35:29 +0000

--- 00001_Iteración II/test.py
+++ 00001_Iteración II/test.py
@@ -2,3 +2,6 @@ class TestFixtures(unittest.TestCase):
   
   def test_listas(self):
     self.assertFalse(False in (set([x for x in profesores_dh if x in ('Pablo', 'Demian', 'Paolo', 'Julian')])), 'Falta algun nombre en la lista')
+    
+  def test_iteracion(self):
+    self.assertTrue(nombre=='Julian', 'La iteracion no se realizo correctamente.')
