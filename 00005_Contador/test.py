e989f37874b337ca8b6c7ee7ed65a10fde95dc23
Andres Furlan <Andres Furlan>
2019-02-06 23:57:29 +0000

    Mumuki Export on 2019-02-06 23:57:29 +0000

--- 00005_Contador/test.py
+++ 00005_Contador/test.py
@@ -1,3 +1,6 @@
 class TestFixtures(unittest.TestCase):
   def test_listas(self):
     self.assertEquals(contador, 8, 'El valor de contador no es correcto')
+    
+  def test_iteracion(self):
+    self.assertTrue(n==6)
