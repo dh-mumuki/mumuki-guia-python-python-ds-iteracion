72de3668e07ea037307cdff20ec9bdda6943febd
Andres Furlan <Andres Furlan>
2019-02-06 23:58:33 +0000

    Mumuki Export on 2019-02-06 23:58:33 +0000

--- 00005_Contador/test.py
+++ 00005_Contador/test.py
@@ -3,4 +3,5 @@ class TestFixtures(unittest.TestCase):
     self.assertEquals(contador, 8, 'El valor de contador no es correcto')
     
   def test_iteracion(self):
+    self.assertTrue(isinstance(n, (int)), 'Tiene que existir la asignacion de los valores a n durante la iteracion.')
     self.assertTrue(n==6)
