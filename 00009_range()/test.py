6326b40458afba7938b73b5f769c211cdcbeb729
Andres Furlan <Andres Furlan>
2019-02-06 23:51:15 +0000

    Mumuki Export on 2019-02-06 23:51:15 +0000

--- 00009_range()/test.py
+++ 00009_range()/test.py
@@ -2,4 +2,4 @@
 class TestFixtures(unittest.TestCase):
   
   def test_contenido(self):
-    self.assertEqual(i, 3, 'La iteracion no es correcta.')
+    self.assertTrue(i== 3, 'La iteracion no es correcta.')
