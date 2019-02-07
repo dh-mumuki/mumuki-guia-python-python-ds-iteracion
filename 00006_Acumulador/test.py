feabf1bfeee9540a44aa67af1743419542cf7aa6
Andres Furlan <Andres Furlan>
2019-02-07 00:02:51 +0000

    Mumuki Export on 2019-02-07 00:02:51 +0000

--- 00006_Acumulador/test.py
+++ 00006_Acumulador/test.py
@@ -1,5 +1,6 @@
 class TestFixtures(unittest.TestCase):
-  def test_listas(self):
+  def test_sumatoria(self):
+    self.assertTrue(isinstance(sumatoria,(int, float)), 'El valor de la sumatoria no es numerico')
     self.assertEquals(sumatoria, 42, 'El valor de sumatoria es incorrecto.')
     
   def test_iteracion(self):
