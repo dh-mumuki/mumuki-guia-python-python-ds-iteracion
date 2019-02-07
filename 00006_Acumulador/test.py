7e76d3c290f0cd16ca718fc8ca556161b58c3e1c
Andres Furlan <Andres Furlan>
2019-02-07 00:00:42 +0000

    Mumuki Export on 2019-02-07 00:00:42 +0000

--- 00006_Acumulador/test.py
+++ 00006_Acumulador/test.py
@@ -1,3 +1,7 @@
 class TestFixtures(unittest.TestCase):
   def test_listas(self):
     self.assertEquals(sumatoria, 42)
+    
+  def test_iteracion(self):
+    self.assertTrue(isinstance(n, (int)), 'Tiene que existir la asignacion de los valores a n durante la iteracion.')
+    self.assertTrue(n==6)
